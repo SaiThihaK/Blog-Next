559ce777c1f9f462350b39e24d376ddcf34db5b1
Sai <saithihak2@gmail.com>
2024-01-10 03:53:32 +0630

    check tsc

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
+import AuthProvider from "@/providers/AuthProvider";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import AuthProvider from "./providers/authProvider";
 
 const inter = Inter({ subsets: ["latin"] });
 
