bef163bb36826138d896dd8d55e254b08489c17f
HtetZarni <loowmaad@gmail.com>
2024-01-12 02:12:04 +0630

    home page ui developing: featured, category lists and postlists sections completed

---
+++ page-containers/home/menu.tsx
@@ -0,0 +1,27 @@
+import MenuCategories from '@/components/shared/menuCategories';
+import MenuPosts from '@/components/shared/menuPosts';
+import React from 'react';
+
+const Menu = () => {
+  return (
+    <div className="hidden lg:block flex-2 mt-[70px]">
+      <h2 className="text-slate-600 text-sm lg:text-base font-medium">
+        {"What's hot?"}
+      </h2>
+      <h1 className="text-base lg:text-lg">Most Popular</h1>
+      <MenuPosts withImages={false} />
+      <h2 className="text-slate-600 text-sm lg:text-base font-medium">
+        Discover by topics
+      </h2>
+      <h1 className="text-base lg:text-lg">Categores</h1>
+      <MenuCategories />
+      <h2 className="text-slate-600 text-sm lg:text-base font-medium">
+        Chosen by the editors
+      </h2>
+      <h1 className="text-base lg:text-lg">Editor Picks</h1>
+      <MenuPosts />
+    </div>
+  );
+};
+
+export default Menu;
