f7ccf316d316972a3f687d7bab19b79245b2e426
Sai <saithihak2@gmail.com>
2024-01-13 14:45:45 +0630

    current user action

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,7 +1,8 @@
-import Login from '@/page-containers/login';
-import React from 'react';
+import Login from "@/page-containers/login";
+import { NextPage } from "next";
+import React from "react";
 
-const Page = () => {
+const Page: NextPage = () => {
   return <Login />;
 };
 
