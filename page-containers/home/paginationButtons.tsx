57cc5e96d59ee4bee8c1a9c5943ec469af45af79
HtetZarni <loowmaad@gmail.com>
2024-01-13 14:05:19 +0630

    add pagination button

---
+++ page-containers/home/paginationButtons.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import {
+  Pagination,
+  PaginationContent,
+  PaginationEllipsis,
+  PaginationItem,
+  PaginationLink,
+  PaginationNext,
+  PaginationPrevious,
+} from '@/components/ui/pagination';
+
+const PaginationButtons = () => {
+  return (
+    <div>
+      <Pagination>
+        <PaginationContent className="justify-between w-full">
+          <PaginationItem>
+            <PaginationPrevious href="#" />
+          </PaginationItem>
+          <PaginationItem>
+            <PaginationNext href="#" />
+          </PaginationItem>
+        </PaginationContent>
+      </Pagination>
+    </div>
+  );
+};
+
+export default PaginationButtons;
