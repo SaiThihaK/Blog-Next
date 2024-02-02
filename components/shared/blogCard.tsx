d79cc961b04a00d9904781060e8bde81cdbf9f01
Sai <saithihak2@gmail.com>
2024-02-02 14:12:48 +0630

    feature/blog

--- components/shared/blogCard.tsx
+++ components/shared/blogCard.tsx
@@ -1,7 +1,7 @@
-import { formatDate } from '@/lib/utils';
-import Image from 'next/image';
-import Link from 'next/link';
-import React from 'react';
+import { formatDate } from "@/lib/utils";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
 
 interface Props {
   id: string;
@@ -44,7 +44,7 @@ const PostCard: React.FC<Props> = ({
           className="text-slate-600 overflow-hidden group hover:text-slate-900 transition-all py-1 w-max relative"
         >
           Read More
-          <div className="absolute bottom-0 h-[0.5px] transition-all left-0 -translate-x-full group-hover:translate-x-0 inset-x-0 bg-slate-950 -mt-1"></div>
+          <div className="absolute bottom-0 h-[1px] transition-all left-0 -translate-x-full group-hover:translate-x-0 inset-x-0 bg-slate-950 -mt-1"></div>
         </Link>
       </div>
     </div>
